c6495406a67da96b053d2471d76e5bfd7298429f
im-ajax <saniahmed0480@gmail.com>
2026-08-02 15:57:01 +0600

    added all the functions

--- jsFiles/changer.js
+++ jsFiles/changer.js
@@ -5,36 +5,48 @@
     totalCount.innerText=cardCounter;
     jobCount.innerText=cardCounter;
 
+// button selector
+// needs to be revised
+document.getElementById("selector-1").addEventListener('click',function(){
+    selectorColor('selector-2','selector-3');
+})
+document.getElementById("selector-2").addEventListener('click',function(){
+    selectorColor('selector-1','selector-3');
+})
+document.getElementById("selector-3").addEventListener('click',function(){
+    selectorColor('selector-1','selector-2');
+})
+
 
 //card 1
 document.getElementById('job-1-btn').addEventListener('click',function(){
     btnChanger('job-1-apply','newBtn-1-adder','job-1-btn');
 });
 //card-2
-document.getElementById('job-1-btn').addEventListener('click',function(){
+document.getElementById('job-2-btn').addEventListener('click',function(){
     btnChanger('job-2-apply','newBtn-2-adder','job-2-btn');
 });
 //card-3
-document.getElementById('job-1-btn').addEventListener('click',function(){
+document.getElementById('job-3-btn').addEventListener('click',function(){
     btnChanger('job-3-apply','newBtn-3-adder','job-3-btn');
 });
 //card-4
-document.getElementById('job-1-btn').addEventListener('click',function(){
+document.getElementById('job-4-btn').addEventListener('click',function(){
     btnChanger('job-4-apply','newBtn-4-adder','job-4-btn');
 });
 //card-5
-document.getElementById('job-1-btn').addEventListener('click',function(){
+document.getElementById('job-5-btn').addEventListener('click',function(){
     btnChanger('job-5-apply','newBtn-5-adder','job-5-btn');
 });
 //card-6
-document.getElementById('job-1-btn').addEventListener('click',function(){
+document.getElementById('job-6-btn').addEventListener('click',function(){
     btnChanger('job-6-apply','newBtn-6-adder','job-6-btn');
 });
 //card-7
-document.getElementById('job-1-btn').addEventListener('click',function(){
+document.getElementById('job-7-btn').addEventListener('click',function(){
     btnChanger('job-7-apply','newBtn-7-adder','job-7-btn');
 });
 //card-8
-document.getElementById('job-1-btn').addEventListener('click',function(){
+document.getElementById('job-8-btn').addEventListener('click',function(){
     btnChanger('job-8-apply','newBtn-8-adder','job-8-btn');
 });
--- jsFiles/machine.js
+++ jsFiles/machine.js
@@ -17,3 +17,12 @@ function btnChanger(id1,id2,id3){
     document.getElementById(id2).appendChild(statusBox);
     document.getElementById(id3).disabled='true';
 }
+//items selector color pallete
+// needs rework
+function selectorColor(id1,id2){
+    const btn=document.getElementById(id1);
+    btn.style.backgroundColor='white';
+    btn.style.color='black';
+    btn.style.border='1px solid black';
+    selectorColor(id2)
+}
